3ac418d1722d24ae8170e0458b4bd6ddbcf07aff
Intanptsr <167785513+Intanptsr@users.noreply.github.com>
2024-04-25 20:38:46 +0700

    Add files via upload

---
+++ src/main/java/com/example/page1/Intent.java
@@ -0,0 +1,15 @@
+package com.example.page1;
+
+import android.app.Activity;
+
+public class Intent extends android.content.Intent {
+    public Intent(regact mainActivity, Class<page5> page2Class) {
+    }
+
+    public Intent(Activity page2, Class<page2> page2Class) {
+
+    }
+
+    public Intent(page3 page3, Class<page5> page5Class) {
+    }
+}
